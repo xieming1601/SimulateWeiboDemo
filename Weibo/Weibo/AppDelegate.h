f467642f8b140e4f61cd751585546f0611ee31ab
xsm <xieming1601@163.com>
2016-10-10 14:46:31 +0800

    empty project

---
+++ Weibo/Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by xsm on 2016/10/10.
+//  Copyright © 2016年 xsm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
